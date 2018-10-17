b9c58f34e5e3f713f22580a0fe9861ddc514e58e
Josselin Auguste <jauguste@iblop.net>
2018-10-17 11:38:43 +0200

    Cleanup code

--- transfer.py
+++ transfer.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-import httplib2
-import googleapiclient.discovery
-import googleapiclient.http
-import googleapiclient.errors
-import oauth2client.client
-import sys
-import pprint
 import os
+import pprint
+import sys
+
+import googleapiclient.discovery
+import googleapiclient.errors
+import googleapiclient.http
+import httplib2
+import oauth2client.client
+
 
 def get_drive_service():
     OAUTH2_SCOPE = 'https://www.googleapis.com/auth/drive'
@@ -26,12 +28,14 @@ def get_drive_service():
     drive_service = googleapiclient.discovery.build('drive', 'v2', http=http)
     return drive_service
 
+
 def get_permission_id_for_email(service, email):
     try:
         id_resp = service.permissions().getIdForEmail(email=email).execute()
         return id_resp['id']
     except googleapiclient.errors.HttpError as e:
         print('An error occured: {}'.format(e))
+
 
 def show_info(service, drive_item, prefix, permission_id):
     try:
@@ -41,10 +45,10 @@ def show_info(service, drive_item, prefix, permission_id):
         print('No title for this item:')
         pprint.pprint(drive_item)
 
-def grant_ownership(service, drive_item, prefix, permission_id, show_already_owned):
-    full_path = os.path.join(os.path.sep.join(prefix), drive_item['title']).encode('utf-8', 'replace')
 
-    #pprint.pprint(drive_item)
+def grant_ownership(service, drive_item, prefix, permission_id, show_already_owned):
+    full_path = os.path.join(os.path.sep.join(prefix), drive_item['title']).encode('utf-8',
+                                                                                   'replace')
 
     current_user_owns = False
     for owner in drive_item['owners']:
@@ -62,10 +66,12 @@ def grant_ownership(service, drive_item, prefix, permission_id, show_already_own
         return
 
     try:
-        permission = service.permissions().get(fileId=drive_item['id'], permissionId=permission_id).execute()
+        permission = service.permissions().get(fileId=drive_item['id'],
+                                               permissionId=permission_id).execute()
         permission['role'] = 'owner'
         print('    Upgrading existing permissions to ownership.')
-        return service.permissions().update(fileId=drive_item['id'], permissionId=permission_id, body=permission, transferOwnership=True).execute()
+        return service.permissions().update(fileId=drive_item['id'], permissionId=permission_id,
+                                            body=permission, transferOwnership=True).execute()
     except googleapiclient.errors.HttpError as e:
         if e.resp.status != 404:
             print('An error occurred updating ownership permissions: {}'.format(e))
@@ -76,11 +82,14 @@ def grant_ownership(service, drive_item, prefix, permission_id, show_already_own
                   'type': 'user',
                   'id': permission_id}
     try:
-        service.permissions().insert(fileId=drive_item['id'], body=permission, emailMessage='Automated recursive transfer of ownership.').execute()
+        service.permissions().insert(fileId=drive_item['id'], body=permission,
+                                     emailMessage='Automated recursive transfer of ownership.').execute()
     except googleapiclient.errors.HttpError as e:
         print('An error occurred inserting ownership permissions: {}'.format(e))
 
-def process_all_files(service, callback=None, callback_args=None, minimum_prefix=None, current_prefix=None, folder_id='root'):
+
+def process_all_files(service, callback=None, callback_args=None, minimum_prefix=None,
+                      current_prefix=None, folder_id='root'):
     if minimum_prefix is None:
         minimum_prefix = []
     if current_prefix is None:
@@ -99,23 +108,26 @@ def process_all_files(service, callback=None, callback_args=None, minimum_prefix
             children = service.children().list(folderId=folder_id, **param).execute()
             for child in children.get('items', []):
                 item = service.files().get(fileId=child['id']).execute()
-                #pprint.pprint(item)
                 if item['kind'] == 'drive#file':
                     if current_prefix[:len(minimum_prefix)] == minimum_prefix:
-                        print(u'File: {} ({}, {})'.format(item['title'], current_prefix, item['id']))
+                        print(
+                            u'File: {} ({}, {})'.format(item['title'], current_prefix, item['id']))
                         callback(service, item, current_prefix, **callback_args)
                     if item['mimeType'] == 'application/vnd.google-apps.folder':
-                        print(u'Folder: {} ({}, {})'.format(item['title'], current_prefix, item['id']))
+                        print(u'Folder: {} ({}, {})'.format(item['title'], current_prefix,
+                                                            item['id']))
                         next_prefix = current_prefix + [item['title']]
                         comparison_length = min(len(next_prefix), len(minimum_prefix))
                         if minimum_prefix[:comparison_length] == next_prefix[:comparison_length]:
-                            process_all_files(service, callback, callback_args, minimum_prefix, next_prefix, item['id'])
+                            process_all_files(service, callback, callback_args, minimum_prefix,
+                                              next_prefix, item['id'])
             page_token = children.get('nextPageToken')
             if not page_token:
                 break
         except googleapiclient.errors.HttpError as e:
             print('An error occurred: {}'.format(e))
             break
+
 
 if __name__ == '__main__':
     if sys.version_info[0] > 2:
@@ -125,12 +137,14 @@ if __name__ == '__main__':
     else:
         minimum_prefix = sys.argv[1].decode('utf-8')
         new_owner = sys.argv[2].decode('utf-8')
-        show_already_owned = False if len(sys.argv) > 3 and sys.argv[3].decode('utf-8') == 'false' else True
+        show_already_owned = False if len(sys.argv) > 3 and sys.argv[3].decode(
+            'utf-8') == 'false' else True
     print('Changing all files at path "{}" to owner "{}"'.format(minimum_prefix, new_owner))
     minimum_prefix_split = minimum_prefix.split(os.path.sep)
     print('Prefix: {}'.format(minimum_prefix_split))
     service = get_drive_service()
     permission_id = get_permission_id_for_email(service, new_owner)
     print('User {} is permission ID {}.'.format(new_owner, permission_id))
-    process_all_files(service, grant_ownership, {'permission_id': permission_id, 'show_already_owned': show_already_owned }, minimum_prefix_split)
-    #print(files)
+    process_all_files(service, grant_ownership,
+                      {'permission_id': permission_id, 'show_already_owned': show_already_owned},
+                      minimum_prefix_split)
